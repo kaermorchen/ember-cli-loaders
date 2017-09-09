5dcc33e6d41e7f85d9b67b6bab870069474cc1d0
Stanislav Romanov <kaermorchen@gmail.com>
2017-09-09 19:56:43 +0300

    Added computed prperty divs

--- addon/components/loader-base.js
+++ addon/components/loader-base.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/loader-base';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
   layout,
   classNames: ['loader-inner'],
 
-  divCount: null
+  divCount: null,
+
+  divs: computed('divCount', function () {
+    return new Array(this.this.get('divCount'));
+  })
 });
