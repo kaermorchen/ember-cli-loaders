333f1b46da5d7f57b465fc55f4b50a31598ee926
Stanislav Romanov <kaermorchen@gmail.com>
2017-09-13 06:44:03 +0300

    Refactored dummy application tempate

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  componentNames: [
+    'loader-ball-pulse',
+    'loader-ball-grid-pulse',
+    'loader-ball-clip-rotate',
+    'loader-ball-clip-rotate-pulse',
+    'loader-square-spin',
+    'loader-ball-clip-rotate-multiple',
+    'loader-ball-pulse-rise',
+    'loader-ball-rotate',
+    'loader-cube-transition',
+    'loader-ball-zig-zag',
+    'loader-ball-zig-zag-deflect',
+    'loader-ball-triangle-path',
+    'loader-ball-scale',
+    'loader-line-scale',
+    'loader-line-scale-party',
+    'loader-ball-scale-multiple',
+    'loader-ball-pulse-sync',
+    'loader-ball-beat',
+    'loader-line-scale-pulse-out',
+    'loader-line-scale-pulse-out-rapid',
+    'loader-ball-scale-ripple',
+    'loader-ball-scale-ripple-multiple',
+    'loader-ball-spin-fade-loader',
+    'loader-line-spin-fade-loader',
+    'loader-triangle-skew-spin',
+    'loader-pacman',
+    'loader-ball-grid-beat',
+    'loader-semi-circle-spin'
+  ]
+});
