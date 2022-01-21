e98df144c0cf5e80fe849bf4911ffc293f52a27b
Stanislav Romanov <kaermorchen@gmail.com>
2022-01-21 19:39:13 +0300

    Add more tests

--- tests/integration/components/loader-ball-beat-test.js
+++ tests/integration/components/loader-ball-beat-test.js
@@ -3,29 +3,29 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | loader-base', function (hooks) {
+module('Integration | Component | loader-ball-beat', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    await render(hbs`<LoaderBase />`);
+  test('It renders', async function (assert) {
+    await render(hbs`<LoaderBallBeat />`);
 
     assert.dom(this.element).hasText('');
   });
 
   test('Component has html class as its component class', async function (assert) {
-    await render(hbs`<LoaderBase />`);
+    await render(hbs`<LoaderBallBeat />`);
 
-    assert.dom(this.element.firstChild).hasClass('loader-base');
+    assert.dom(this.element.firstChild).hasClass('ball-beat');
   });
 
   test('Component html divs equal the component divCount value', async function (assert) {
-    await render(hbs`<LoaderBase />`);
+    await render(hbs`<LoaderBallBeat />`);
 
-    assert.equal(this.element.firstChild.children.length, 0);
+    assert.equal(this.element.firstChild.children.length, 3);
   });
 
   test('all children elemetns are `div`', async function (assert) {
-    await render(hbs`<LoaderBase />`);
+    await render(hbs`<LoaderBallBeat />`);
 
     const hasWrongElements = Array.from(this.element.firstChild.children).some(
       (element) => !(element instanceof HTMLDivElement)
